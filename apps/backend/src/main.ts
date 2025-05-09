9479c751122ffb3ea1a336c37ad2e1af619ebc74
Hussein Aljanaby <husseinalaa.aljanaby@gmail.com>
2025-05-09 13:04:55 +1200

    Added GitHub testing workflow

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
