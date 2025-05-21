a91622022b350c488310084a91a506afec83804b
Kobe Joaquin <kobej234@gmail.com>
2025-05-21 20:37:56 +1200

    Add function to view articles

--- apps/backend/src/api/articles/article.service.ts
+++ apps/backend/src/api/articles/article.service.ts
@@ -6,21 +6,29 @@ import { CreateArticleDto } from './create-article.dto';
 
 @Injectable()
 export class ArticleService {
-  constructor(@InjectModel(Article.name) private articleModel: Model<ArticleDocument>) {}
-
+  constructor(
+    @InjectModel(Article.name) private articleModel: Model<ArticleDocument>,
+  ) {}
   async create(dto: CreateArticleDto): Promise<Article> {
     return await this.articleModel.create(dto);
   }
-
   async findAll(): Promise<Article[]> {
     return await this.articleModel.find().exec();
   }
-
+  async findById(id: string): Promise<Article | null> {
+    return await this.articleModel.findById(id).exec();
+  }
+  async findReviewed(): Promise<Article[]> {
+    return await this.articleModel.find({ isModerated: true }).exec();
+  }
   async findUnmoderated(): Promise<Article[]> {
     return await this.articleModel.find({ isModerated: false }).exec();
   }
-
   async moderate(id: string): Promise<Article | null> {
-    return await this.articleModel.findByIdAndUpdate(id, { isModerated: true }, { new: true });
+    return await this.articleModel.findByIdAndUpdate(
+      id,
+      { isModerated: true },
+      { new: true },
+    );
   }
 }
